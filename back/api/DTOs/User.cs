d3268ec298432fadfeddb440d7c15228641bb36a
Veeti K <veetik@protonmail.com>
2022-03-09 21:05:24 +0200

    UserToReturn dto

---
+++ back/api/DTOs/User.cs
@@ -0,0 +1,14 @@
+﻿using api.Models;
+
+namespace api.DTOs;
+
+public record UserToReturn
+{
+  public Guid Id { get; init; }
+  public string Name { get; init; }
+  public string Email { get; init; }
+  public string PhoneNumber { get; init; }
+  public DateTime CreatedAt { get; init; }
+  public DateTime LastAccessedAt { get; init; }
+  public ICollection<Address> Addresses { get; init; }
+}
