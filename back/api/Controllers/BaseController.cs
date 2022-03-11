2f589f3344a54b6d4a9d9dd606d60a9cad86b4e7
Veeti K <veetik@protonmail.com>
2022-03-11 16:20:01 +0200

    add GetUserId func to controllers

---
+++ back/api/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+public abstract class BaseController : ControllerBase
+{
+  protected Guid? GetUserId()
+  {
+    var success = Guid.TryParse(this.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value, out var userId);
+
+    if (!success) return null;
+
+    return userId;
+  }
+}
