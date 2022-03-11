0e1caeadca1663b1f5828a5dab7526dace55fc2b
Veeti K <veetik@protonmail.com>
2022-03-11 16:17:53 +0200

    phonenumber as null

--- back/api/DTOs/User.cs
+++ back/api/DTOs/User.cs
@@ -7,7 +7,7 @@ public record UserToReturn
   public Guid Id { get; init; }
   public string Name { get; init; }
   public string Email { get; init; }
-  public string PhoneNumber { get; init; }
+  public string? PhoneNumber { get; init; }
   public DateTime CreatedAt { get; init; }
   public DateTime LastAccessedAt { get; init; }
   public ICollection<Address> Addresses { get; init; }
