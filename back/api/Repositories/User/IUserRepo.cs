90e52b69e2407d792b36c470e6c67f72a73a3eb8
Veeti K <veetik@protonmail.com>
2022-03-12 14:02:59 +0200

    Update IUserRepo.cs

--- back/api/Repositories/User/IUserRepo.cs
+++ back/api/Repositories/User/IUserRepo.cs
@@ -2,7 +2,7 @@
 
 public interface IUserRepo
 {
-  public Task<Models.User?> GetOneByPhoneNumber(string aPhoneNumber);
+  public Task<Models.User?> GetOneByPhoneNumber(string? aPhoneNumber);
   public Task<Models.User?> GetOneById(Guid aId);
   public Task<Models.User?> GetOneByEmail(string aEmail);
   public Task Add(Models.User user);
