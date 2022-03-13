99e7a58d2de8034d11a02841175149f5b52a66cb
Veeti K <veetik@protonmail.com>
2022-03-13 18:56:35 +0200

    Modified user repo

--- back/api/Repositories/User/IUserRepo.cs
+++ back/api/Repositories/User/IUserRepo.cs
@@ -1,11 +1,16 @@
-﻿namespace api.Repositories.User;
+﻿using System.Linq.Expressions;
+using api.DTOs.Auth;
+
+namespace api.Repositories.User;
 
 public interface IUserRepo
 {
-  public Task<Models.User?> GetOneByPhoneNumber(string? aPhoneNumber);
-  public Task<Models.User?> GetOneById(Guid aId);
-  public Task<Models.User?> GetOneByEmail(string aEmail);
-  public Task Add(Models.User user);
-  public Task Delete(Models.User user);
-  public Task Delete(Guid? aId);
+  public Task<Models.User?> GetOneByFilter(Expression<Func<Models.User, bool>> aFilter);
+  public Task<IEnumerable<Models.User?>> GetManyByFilter(Expression<Func<Models.User, bool>> aFilter);
+  public Task<Models.User?> GetByPhoneNumber(string aPhoneNumber);
+  public Task<Models.User?> GetByEmail(string aEmail);
+  public Task<Models.User?> GetById(Guid aId);
+  public Task<Models.User> Create(RegisterDTO aDto);
+  public Task Remove(Models.User user);
+  public Task Remove(Guid? aId);
 }
--- back/api/Repositories/User/UserRepo.cs
+++ back/api/Repositories/User/UserRepo.cs
@@ -1,5 +1,9 @@
-﻿using api.Data;
+﻿using System.Linq.Expressions;
+using api.Data;
+using api.DTOs.Auth;
+using api.Exceptions;
 using api.Models;
+using api.Utils;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repositories.User;
@@ -39,36 +43,63 @@ public class UserRepo : IUserRepo
       });
   }
 
-  public async Task<Models.User?> GetOneByPhoneNumber(string? aPhoneNumber)
+  public async Task<Models.User?> GetOneByFilter(Expression<Func<Models.User, bool>> aFilter)
   {
-    if (aPhoneNumber == null) return null;
-
-    return await _populatedUsers.FirstOrDefaultAsync(u => u.PhoneNumber == aPhoneNumber);
+    return await _populatedUsers.Where(aFilter).FirstOrDefaultAsync();
   }
 
-  public async Task<Models.User?> GetOneById(Guid aId)
+  public async Task<IEnumerable<Models.User?>> GetManyByFilter(Expression<Func<Models.User, bool>> aFilter)
   {
-    return await _populatedUsers.FirstOrDefaultAsync(u => u.Id == aId);
+    return await _populatedUsers.Where(aFilter).ToListAsync();
   }
 
-  public async Task<Models.User?> GetOneByEmail(string aEmail)
+  public async Task<Models.User?> GetByPhoneNumber(string aPhoneNumber)
   {
-    return await _populatedUsers.FirstOrDefaultAsync(u => u.Email == aEmail);
+    return await GetOneByFilter(user => user.PhoneNumber == aPhoneNumber);
   }
 
-  public async Task Add(Models.User user)
+  public async Task<Models.User?> GetByEmail(string aEmail)
   {
-    await _context.AddAsync(user);
+    return await GetOneByFilter(user => user.Email == aEmail);
+  }
+
+  public async Task<Models.User?> GetById(Guid aId)
+  {
+    return await GetOneByFilter(user => user.Id == aId);
+  }
+
+  public async Task<Models.User> Create(RegisterDTO aDto)
+  {
+    if (await GetByEmail(aDto.Email) != null)
+      throw new BadRequestException("Email in use");
+
+    if (await GetByPhoneNumber(aDto.PhoneNumber) != null)
+      throw new BadRequestException("Phone number in use");
+
+    Models.User newUser = new()
+    {
+      Id = Guid.NewGuid(),
+      Email = aDto.Email,
+      Name = $"{aDto.FirstName} {aDto.LastName}",
+      PhoneNumber = aDto.PhoneNumber,
+      isTestAccount = aDto.isTestAccount,
+      Password = Hashing.HashToString(aDto.Password),
+      CreatedAt = DateTime.UtcNow.ToString("o")
+    };
+
+    _context.Add(newUser);
     await _context.SaveChangesAsync();
-  }
 
-  public async Task Delete(Models.User user)
+    return newUser;
+  } 
+
+  public async Task Remove(Models.User user)
   {
     _context.Remove(user);
     await _context.SaveChangesAsync();
   }
 
-  public async Task Delete(Guid? aId)
+  public async Task Remove(Guid? aId)
   {
     if (aId == null) return;
     
