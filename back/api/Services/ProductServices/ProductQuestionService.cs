db3e742c6cc5f29d2825f381ef43f6974975f327
Veeti K <veetik@protonmail.com>
2022-03-20 13:36:01 +0200

    use productrepo instead of service

--- back/api/Services/ProductServices/ProductQuestionService.cs
+++ back/api/Services/ProductServices/ProductQuestionService.cs
@@ -11,22 +11,22 @@ namespace api.Services.ProductServices;
 public class ProductQuestionService : IProductQuestionService
 {
   private readonly IMapper _mapper;
-  private readonly IProductService _productService;
+  private readonly IProductRepo _productRepo;
   private readonly IProductQuestionRepo _productQuestionRepo;
 
   public ProductQuestionService(
     IMapper aMapper,
-    IProductService aProductService,
+    IProductRepo aProductRepo,
     IProductQuestionRepo aProductQuestionRepo)
   {
     _mapper = aMapper;
-    _productService = aProductService;
+    _productRepo = aProductRepo;
     _productQuestionRepo = aProductQuestionRepo;
   }
 
   public async Task<ProductQuestionResponse> CreateQuestion(CreateProductQuestionDTO dto, int productId)
   {
-    var product = await _productService.GetById(productId);
+    var product = await _productRepo.GetById(productId);
     if (product is null) throw new NotFoundException("Product not found");
 
     ProductQuestion newQuestion = new()
@@ -44,16 +44,18 @@ public class ProductQuestionService : IProductQuestionService
 
   public async Task<IEnumerable<ProductQuestionResponse>> GetProductQuestions(int productId)
   {
-    var product = await _productService.GetById(productId);
+    var product = await _productRepo.GetById(productId);
     if (product is null) throw new NotFoundException("Product not found");
 
     var questions = await _productQuestionRepo.GetByProductId(productId);
+    if (!questions.Any()) throw new NotFoundException("No questions found");
+    
     return _mapper.Map<IEnumerable<ProductQuestionResponse>>(questions);
   }
 
   public async Task RemoveProductQuestion(int productId, Guid questionId)
   {
-    var product = await _productService.GetById(productId);
+    var product = await _productRepo.GetById(productId);
     if (product is null) throw new NotFoundException("Product not found");
 
     var question = await _productQuestionRepo.GetById(questionId);
