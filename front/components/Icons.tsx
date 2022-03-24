d6cf3649bd04fc7695df98c942dbac4748c4fb01
Veeti K <veetik@protonmail.com>
2022-03-24 03:09:54 +0200

    svg things

--- front/components/Icons.tsx
+++ front/components/Icons.tsx
@@ -2,8 +2,8 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Edit, LogIn, Plus, Trash2, User } from "react-feather";
 import { GiShoppingCart } from "react-icons/gi";
 
-export const TrashIcon = () => <Trash2 transform="scale(0.8)" />;
-export const EditIcon = () => <Edit transform="scale(0.8)" />;
+export const TrashIcon = () => <Trash2 width={14} transform="scale(1.3)" />;
+export const EditIcon = () => <Edit width={14} transform="scale(1.3)" />;
 export const HamburgerIcon = () => <HamburgerMenuIcon style={{ transform: "scale(1.3)" }} />;
 export const ShoppingCartIcon = () => <GiShoppingCart transform="scale(1.9)" />;
 export const UserIcon = User;
