6dabb5465cf7307214619aedf262d958d67ef399
Veeti K <veetik@protonmail.com>
2022-03-24 22:39:11 +0200

    animated chevron icon

--- front/components/Icons.tsx
+++ front/components/Icons.tsx
@@ -1,6 +1,7 @@
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { ChevronDownIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Edit, LogIn, Plus, Trash2, User } from "react-feather";
 import { GiShoppingCart } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 export const TrashIcon = () => <Trash2 width={14} transform="scale(1.3)" />;
 export const EditIcon = () => <Edit width={14} transform="scale(1.3)" />;
@@ -9,3 +10,15 @@ export const ShoppingCartIcon = () => <GiShoppingCart transform="scale(1.9)" />;
 export const UserIcon = User;
 export const LoginIcon = LogIn;
 export const PlusIcon = Plus;
+
+export const Chevron = ({ open }: { open: boolean }) => {
+  return open ? (
+    <motion.div initial={{ rotate: 0 }} animate={{ rotate: 180 }}>
+      <ChevronDownIcon transform="scale(1.2)" />
+    </motion.div>
+  ) : (
+    <motion.div initial={{ rotate: 180 }} animate={{ rotate: 0 }}>
+      <ChevronDownIcon transform="scale(1.2)" />
+    </motion.div>
+  );
+};
