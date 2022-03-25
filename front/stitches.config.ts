d5592cf8709d93677539131d33e2365c1a0b5949
Veeti K <veetik@protonmail.com>
2022-03-25 02:20:44 +0200

    some shadows to global config

--- front/stitches.config.ts
+++ front/stitches.config.ts
@@ -9,5 +9,10 @@ export const { styled, css, getCssText } = createStitches({
     transitions: {
       buttonHover: "all 0.15s cubic-bezier(0,0,.5,1)",
     },
+    shadows: {
+      shadowFar: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
+      shadowNear: "2px 4px 16px rgb(0 0 0 / 16%)",
+      shadowNearest: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+    },
   },
 });
