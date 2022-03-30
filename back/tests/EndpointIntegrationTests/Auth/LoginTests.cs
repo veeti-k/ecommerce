ec87971640e3d6f7e0b5f97b5540e4b2ebc27aae
Veeti K <veetik@protonmail.com>
2022-03-30 19:54:11 +0300

    auth tests

---
+++ back/tests/EndpointIntegrationTests/Auth/LoginTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using api.Exceptions;
+using api.RequestsAndResponses.Auth.Login;
+using api.Security;
+using FluentAssertions;
+using tests.UtilsForTesting;
+using Xunit;
+
+namespace tests.EndpointIntegrationTests.Auth;
+
+public class LoginTests : AuthIntegrationTest
+{
+  [Fact]
+  public async Task Login_WithCorrectCredentials_Succeeds()
+  {
+    var user = DbSeeding.GetUser(Flags.NO_FLAGS);
+
+    var dto = new LoginDto()
+    {
+      Email = user.Email,
+      Password = user.Password
+    };
+
+    var response = await Login_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var json = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+    json.AccessToken.Length.Should().BeGreaterThan(50);
+  }
+
+  [Fact]
+  public async Task Login_WithInCorrectEmail_ReturnsUserNotFound()
+  {
+    var dto = new LoginDto()
+    {
+      Email = Guid.NewGuid().ToString(),
+      Password = Guid.NewGuid().ToString()
+    };
+
+    var response = await Login_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+    var json = await response.Content.ReadFromJsonAsync<MyExceptionResponse>();
+
+    json.Message.Should().Be("User not found");
+  }
+
+  [Fact]
+  public async Task Login_WithCorrectEmail_WithIncorrectPassword_ReturnsUnauthorized_InvalidPassword()
+  {
+    var user = DbSeeding.GetUser(Flags.NO_FLAGS);
+
+    var dto = new LoginDto()
+    {
+      Email = user.Email,
+      Password = Guid.NewGuid().ToString()
+    };
+
+    var response = await Login_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+    var json = await response.Content.ReadFromJsonAsync<MyExceptionResponse>();
+
+    json.Message.Should().Be("Invalid password");
+  }
+}
---
+++ back/tests/EndpointIntegrationTests/Auth/RegisterTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using api.Exceptions;
+using api.RequestsAndResponses.Auth.Register;
+using api.Security;
+using FluentAssertions;
+using tests.UtilsForTesting;
+using Xunit;
+
+namespace tests.EndpointIntegrationTests.Auth;
+
+public class RegisterTests : AuthIntegrationTest
+{
+  [Fact]
+  public async Task Register_WithUniqueEmail_WithUniquePhoneNumber_Succeeds()
+  {
+    var dto = new RegisterDto()
+    {
+      Email = Guid.NewGuid().ToString(),
+      Name = Guid.NewGuid().ToString(),
+      Password = Guid.NewGuid().ToString(),
+      isTestAccount = false,
+      PhoneNumber = Guid.NewGuid().ToString()
+    };
+
+    var response = await Register_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var json = await response.Content.ReadFromJsonAsync<RegisterResponse>();
+
+    json.AccessToken.Length.Should().BeGreaterThan(50);
+  }
+  
+  [Fact]
+  public async Task Register_WithTakenEmail_ReturnsBadRequest_EmailInUse()
+  {
+    var user = DbSeeding.GetUser(Flags.NO_FLAGS);
+    
+    var dto = new RegisterDto()
+    {
+      Email = user.Email,
+      Name = Guid.NewGuid().ToString(),
+      Password = Guid.NewGuid().ToString(),
+      isTestAccount = false,
+      PhoneNumber = Guid.NewGuid().ToString()
+    };
+
+    var response = await Register_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+    var json = await response.Content.ReadFromJsonAsync<MyExceptionResponse>();
+
+    json.Message.Should().Be("Email already in use");
+  }
+  
+  [Fact]
+  public async Task Register_WithUniqueEmail_WithTakenPhoneNumber_ReturnsBadRequest_PhoneNumberInUse()
+  {
+    var user = DbSeeding.GetUser(Flags.NO_FLAGS);
+    
+    var dto = new RegisterDto()
+    {
+      Email = Guid.NewGuid().ToString(),
+      Name = Guid.NewGuid().ToString(),
+      Password = Guid.NewGuid().ToString(),
+      isTestAccount = false,
+      PhoneNumber = user.PhoneNumber
+    };
+
+    var response = await Register_TEST_REQUEST(dto);
+
+    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+    var json = await response.Content.ReadFromJsonAsync<MyExceptionResponse>();
+
+    json.Message.Should().Be("Phone number already in use");
+  }
+}
