59e8f119357e0c2855ace0c1161c4c1f682406ee
Veeti K <veetik@protonmail.com>
2022-04-09 00:29:04 +0300

    category helpers

---
+++ back/api/Utils/Categories.cs
@@ -0,0 +1,23 @@
+﻿using api.Models.Product;
+
+namespace api.Utils;
+
+public static class Categories
+{
+  public static List<ProductCategory> GetCategoryPath(IEnumerable<ProductCategory> allCategories, int deepestCategoryId)
+  {
+    var result = new List<ProductCategory>();
+
+    var current = allCategories.FirstOrDefault(c => c.Id == deepestCategoryId);
+    
+    result.Add(current);
+
+    while (current.ParentId != null)
+    {
+      current = allCategories.FirstOrDefault(c => c.Id == c.ParentId);
+      result.Add(current);
+    }
+
+    return result;
+  }
+}
