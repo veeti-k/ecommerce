b756f4e7ec53c75ac6dc65be38506ab5d34e2501
Veeti K <veetik@protonmail.com>
2022-04-17 04:26:14 +0300

    Update Markdown.tsx

--- front/components/Markdown.tsx
+++ front/components/Markdown.tsx
@@ -2,10 +2,19 @@ import { Heading, UnorderedList, ListItem } from "@chakra-ui/react";
 import { FC } from "react";
 import { Paragraph, Text } from "./Text";
 import ReactMarkdown from "react-markdown";
+import { styled } from "../stitches.config";
 
 type Props = {
   children: string;
 };
+
+const MarkdownP = styled(Paragraph, {
+  fontSize: "1rem",
+
+  "&:not(:first-child)": {
+    paddingTop: "1rem",
+  },
+});
 
 export const Markdown: FC<Props> = ({ children }) => (
   <ReactMarkdown
@@ -17,9 +26,7 @@ export const Markdown: FC<Props> = ({ children }) => (
           <Text style={{ fontSize: "1rem" }}>{props.children}</Text>
         </ListItem>
       ),
-      p: ({ node, ...props }) => (
-        <Paragraph {...props} style={{ fontSize: "1rem", paddingTop: "1rem" }} />
-      ),
+      p: ({ node, ...props }) => <MarkdownP {...props} />,
     }}
   >
     {children}
