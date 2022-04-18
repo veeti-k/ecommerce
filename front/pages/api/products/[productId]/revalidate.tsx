11324aff3914a088b42751c94048147f9a64f0a2
Veeti K <veetik@protonmail.com>
2022-04-18 14:54:43 +0300

    on-demand page static generation

---
+++ front/pages/api/products/[productId]/revalidate.tsx
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { revalidateSecret } from "../../../../utils/consts";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const secret = req.body.secret;
+  if (!secret || secret !== revalidateSecret) return res.status(401).end();
+
+  const productId = req.query.productId;
+  if (!productId) return res.status(400).json({ error: "Missing productId" });
+
+  try {
+    await res.unstable_revalidate(`/products/${productId}`);
+    await res.unstable_revalidate(`/products/${productId}/reviews`);
+    await res.unstable_revalidate(`/products/${productId}/questions`);
+
+    res.status(200).json({ message: "OK" });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({ error: err.message });
+  }
+}
--- front/utils/consts.ts
+++ front/utils/consts.ts
@@ -1,1 +1,2 @@
 export const apiBase = process.env.NEXT_PUBLIC_API_BASE;
+export const revalidateSecret = process.env.REVALIDATE_SECRET; // not accessible from frontend
