8daa1b9b2343375821aa0bbf7c0dd377acef79c3
Veeti K <veetik@protonmail.com>
2022-04-21 20:53:09 +0300

    props to lower case

--- packages/shared/src/types/User.ts
+++ packages/shared/src/types/User.ts
@@ -2,14 +2,14 @@ import { Address } from "./Address";
 import { Session } from "./Session";
 
 export interface User {
-  UserId: number;
-  Name: string;
-  Email: string;
-  PhoneNumber: string | null;
-  Flags: bigint;
-  CreatedAt: Date;
-  Password: string;
+  userId: number;
+  name: string;
+  email: string;
+  phoneNumber: string | null;
+  flags: bigint;
+  createdAt: Date;
+  password: string;
 
-  Addresses: Address[];
-  Sessions: Session[];
+  addresses: Address[];
+  sessions: Session[];
 }
