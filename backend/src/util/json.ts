8ef14f5a5b258723042f735604a1cf106907e4da
Veeti K <veetik@protonmail.com>
2022-04-23 16:49:01 +0300

    fix test failing

--- backend/src/util/json.ts
+++ backend/src/util/json.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { Endpoint } from "../types/ApiThings";
+import { Middleware } from "../types/ApiThings";
 import { respondError } from "./respondWith";
 
-export const json: Endpoint = (req, res, next) => {
+export const json: Middleware = (req, res, next) => {
   express.json()(req, res, (err: any) => {
     if (err) respondError({ res, statusCode: 400, message: "Invalid JSON" });
     else next();
