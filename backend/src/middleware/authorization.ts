f38b07bfc30f2c26512e5f48c71e713c947adb8e
Veeti K <veetik@protonmail.com>
2022-04-23 18:43:55 +0300

    fix authorization

--- backend/src/middleware/authorization.ts
+++ backend/src/middleware/authorization.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { Middleware } from "../types/ApiThings";
 import { Flags } from "../types/Flags";
 import { User } from "../types/User";
+import { REQ_USER } from "../util/consts";
 import { respondError } from "../util/respondWith";
 
 const errorMissingPerms = (res: Response) =>
@@ -10,11 +11,11 @@ const errorMissingPerms = (res: Response) =>
 export const authorization =
   (...neededFlags: bigint[]): Middleware =>
   (req, res, next) => {
-    const user = req.app.get("user") as User | undefined;
+    const user = req.app.get(REQ_USER) as User | undefined;
 
     if (!user) throw new Error("No user set, did you forget to call authentication middleware?");
 
-    if (user.flags & Flags.Admin) return next(); // Admin has permission to do anything
+    if ((user.flags & Flags.Admin) === Flags.Admin) return next(); // Admin has permission to do anything
 
     const totalNeeded = neededFlags.reduce((acc, cur) => acc | cur);
 
