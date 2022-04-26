7156835083298cfab2f654ec5fb72eb7d4333735
Veeti K <veetik@protonmail.com>
2022-04-26 10:31:23 +0300

    use joi

--- backend/apps/users/src/validators/v1/sessions/remove.ts
+++ backend/apps/users/src/validators/v1/sessions/remove.ts
@@ -1,30 +1,6 @@
-import { Validator, RemoveSessionsRequestBody, ValidationErrors, validateArray } from "shared";
+import Joi from "joi";
 
-export const removeSessionsRequestBodyValidator: Validator<RemoveSessionsRequestBody> = (obj) => {
-  const errors: ValidationErrors<RemoveSessionsRequestBody> = {};
-
-  const sessionIdsResult = validateArray(obj.sessionIds, "sessionIds");
-  if (sessionIdsResult) errors["sessionIds"] = { message: sessionIdsResult };
-  else {
-    const sessionIds = obj.sessionIds as any[];
-    sessionIds.forEach((sessionId, index) => {
-      if (typeof sessionId !== "string")
-        errors[`sessionIds[${index}]`] = {
-          message: `'sessionIds' must be a string array, sessionIds[${index}] is not a string`,
-        };
-    });
-  }
-
-  if (Object.keys(errors).length > 0)
-    return {
-      isValid: false,
-      errors,
-    };
-
-  return {
-    isValid: true,
-    validated: {
-      sessionIds: obj.sessionIds,
-    },
-  };
-};
+export const removeSessionsRequestBodyValidator = (obj: any) =>
+  Joi.object({
+    sessionIds: Joi.array().items(Joi.string().uuid()).required(),
+  }).validate(obj);
