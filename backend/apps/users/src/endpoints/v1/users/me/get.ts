dc15938d30ccde24080025322a253063d3170657
Veeti K <veetik@protonmail.com>
2022-04-27 22:17:58 +0300

    return sessions and addresses from users/me

--- backend/apps/users/src/endpoints/v1/users/me/get.ts
+++ backend/apps/users/src/endpoints/v1/users/me/get.ts
@@ -1,11 +1,14 @@
 import { AuthVerifyUserResponse, Endpoint, REQ_USER, respondSuccess } from "shared";
+import { db } from "../../../../database";
 
-export const get: Endpoint = (req, res) => {
+export const get: Endpoint = async (req, res) => {
   const currentUser = req.app.get(REQ_USER) as AuthVerifyUserResponse;
+
+  const fullUser = await db.user.get.byUserIdWith.sessionsAndAddresses(currentUser.userId);
 
   respondSuccess({
     res,
     statusCode: 200,
-    json: currentUser,
+    json: { ...fullUser, flags: Number(fullUser!.flags) },
   });
 };
