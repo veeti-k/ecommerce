9f979e8d1fd39f20cdf059bda7363c33b192a20c
Veeti K <veetik@protonmail.com>
2022-04-27 22:17:45 +0300

    no password from db

--- backend/apps/users/src/database/methods/user.ts
+++ backend/apps/users/src/database/methods/user.ts
@@ -1,5 +1,33 @@
 import prisma from "../client";
 
+const userSelect = {
+  userId: true,
+  name: true,
+  email: true,
+  createdAt: true,
+  phoneNumber: true,
+  flags: true,
+};
+
+const addressSelect = {
+  addressId: true,
+  city: true,
+  email: true,
+  name: true,
+  phoneNumber: true,
+  state: true,
+  streetAddress: true,
+  zip: true,
+  userId: true,
+};
+
+const sessionSelect = {
+  sessionId: true,
+  userId: true,
+  createdAt: true,
+  lastUsedAt: true,
+};
+
 export const createUserAndSession = async (body: any, hashedPassword: string) => {
   const userWithSessions = await prisma.user.create({
     data: {
@@ -27,17 +55,33 @@ export const createUserAndSession = async (body: any, hashedPassword: string) =>
 };
 
 export const get = {
-  byUserId: async (userId: number) => prisma.user.findFirst({ where: { userId } }),
+  byUserId: async (userId: number) =>
+    prisma.user.findFirst({ where: { userId }, select: userSelect }),
   byUserIdWith: {
     sessions: async (userId: number) =>
-      prisma.user.findFirst({ where: { userId }, include: { sessions: true } }),
+      prisma.user.findFirst({
+        where: { userId },
+        select: { ...userSelect, sessions: { select: { ...sessionSelect } } },
+      }),
+
+    sessionsAndAddresses: async (userId: number) =>
+      prisma.user.findFirst({
+        where: { userId },
+        select: {
+          ...userSelect,
+          sessions: { select: { ...sessionSelect } },
+          addresses: { select: { ...addressSelect } },
+        },
+      }),
   },
-  byEmail: (email: string) => prisma.user.findFirst({ where: { email } }),
-  byPhoneNumber: (phoneNumber: string) => prisma.user.findFirst({ where: { phoneNumber } }),
+  byEmail: (email: string) => prisma.user.findFirst({ where: { email }, select: userSelect }),
+  byPhoneNumber: (phoneNumber: string) =>
+    prisma.user.findFirst({ where: { phoneNumber }, select: userSelect }),
 };
 
 export const update = (userId: number, updated: any) =>
   prisma.user.update({
     where: { userId },
     data: updated,
+    select: userSelect,
   });
