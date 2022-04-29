82b45dbe006bd09d61f21510aa796e0073ed96e2
Veeti K <veetik@protonmail.com>
2022-04-29 21:23:56 +0300

    forgot these here

--- backend/apps/users/src/util/authorization.ts
+++
@@ -1,9 +0,0 @@
-import { Flags } from "shared";
-
-export const hasAccess = (neededFlags: bigint[], userFlags: bigint) => {
-  const totalNeeded = neededFlags.reduce((acc, curr) => acc | curr);
-
-  return isAdmin(userFlags) || (userFlags & totalNeeded) === totalNeeded;
-};
-
-export const isAdmin = (userFlags: bigint) => (userFlags & Flags.Admin) === Flags.Admin;
