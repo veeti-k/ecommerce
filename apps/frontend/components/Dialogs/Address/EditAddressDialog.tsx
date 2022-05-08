6630d2513591cdf49fbfa3b07e56a1c98ef0376d
Veeti K <veetik@protonmail.com>
2022-05-08 10:28:08 +0300

    update edit address form and dialog

--- apps/frontend/components/Dialogs/Address/EditAddressDialog.tsx
+++ apps/frontend/components/Dialogs/Address/EditAddressDialog.tsx
@@ -1,73 +1,28 @@
-import { IconButton, Input, Tooltip, useDisclosure } from "@chakra-ui/react";
-import { useContext, useEffect, useState } from "react";
-import { InputLabelContainer } from "../../Containers";
+import { IconButton, Tooltip, useDisclosure } from "@chakra-ui/react";
+import { useContext } from "react";
 import { EditIcon } from "../../Icons";
-import { FlexDiv } from "../../Containers";
 import { toast } from "react-hot-toast";
 import { UserContext } from "../../../UserProvider/provider";
 import { EditAddressRequest } from "../../../utils/Requests/Address";
 import { Address } from "../../../types/User";
-import { Dialog } from "../Dialog";
+import { Dialog2, DialogBody, DialogHeader } from "../Dialog";
 import { GetMe } from "../../../utils/Requests/Account";
+import { EditAddressForm } from "../../Forms/EditAddressForm";
 
 export const EditAddressDialog = ({ address }: { address: Address }) => {
-  const [name, setName] = useState<string>("");
-  const [phoneNumber, setPhoneNumber] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [streetAddress, setStreetAddress] = useState<string>("");
-  const [city, setCity] = useState<string>("");
-  const [zip, setZip] = useState<string>("");
-  const [state, setState] = useState<string>("");
-
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { dispatch } = useContext(UserContext);
 
-  const onSubmit = async () => {
-    const notifId = toast.loading("Editing address");
-
-    const res = await EditAddressRequest(address.addressId, {
-      name,
-      phoneNumber,
-      email,
-      streetAddress,
-      city,
-      zip,
-      state,
-    });
-
-    toast.dismiss(notifId);
+  const onSubmit = async (values: any) => {
+    const res = await EditAddressRequest(address.addressId, values);
 
     if (res) {
       toast.success("Address edited");
       GetMe(dispatch);
-
-      setName("");
-      setPhoneNumber("");
-      setEmail("");
-      setStreetAddress("");
-      setCity("");
-      setZip("");
-      setState("");
-
       onClose();
     }
   };
-
-  const submitDisabled =
-    !name || !phoneNumber || !email || !streetAddress || !city || !zip || !state;
-
-  useEffect(() => {
-    if (!address) return;
-
-    setName(address.name);
-    setPhoneNumber(address.phoneNumber);
-    setEmail(address.email);
-    setStreetAddress(address.streetAddress);
-    setCity(address.city);
-    setZip(address.zip);
-    setState(address.state);
-  }, [address]);
 
   return (
     <>
@@ -81,89 +36,13 @@ export const EditAddressDialog = ({ address }: { address: Address }) => {
         />
       </Tooltip>
 
-      <Dialog
-        header="Edit address"
-        submitLabel="Update"
-        submitDisabled={submitDisabled}
-        onSubmit={onSubmit}
-        isOpen={isOpen}
-        onClose={onClose}
-        onOpen={onOpen}
-      >
-        {" "}
-        <FlexDiv column>
-          <InputLabelContainer id="name" label="Name">
-            <Input
-              id="name"
-              type="name"
-              autoComplete="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="phone-number" label="Phone number">
-            <Input
-              id="phone-number"
-              type="tel"
-              autoComplete="tel"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="email" label="Email">
-            <Input
-              id="email"
-              type="email"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="street-address" label="Street address">
-            <Input
-              id="street-address"
-              type="text"
-              autoComplete="street-address"
-              value={streetAddress}
-              onChange={(e) => setStreetAddress(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="city" label="City">
-            <Input
-              id="city"
-              type="text"
-              autoComplete="address-level2"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="zip" label="Zip code">
-            <Input
-              id="zip"
-              type="text"
-              autoComplete="postal-code"
-              value={zip}
-              onChange={(e) => setZip(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-          <InputLabelContainer id="state" label="State">
-            <Input
-              id="state"
-              type="text"
-              autoComplete="address-level1"
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-              required
-            />
-          </InputLabelContainer>
-        </FlexDiv>
-      </Dialog>
+      <Dialog2 isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
+        <DialogHeader>Edit address</DialogHeader>
+
+        <DialogBody>
+          <EditAddressForm onSubmit={onSubmit} address={address} />
+        </DialogBody>
+      </Dialog2>
     </>
   );
 };
---
+++ apps/frontend/components/Forms/EditAddressForm.tsx
@@ -0,0 +1,189 @@
+import { Formik } from "formik";
+import { useContext } from "react";
+import { BreakpointContext } from "../../BreakpointProvider/BreakpointProvider";
+import { Address } from "../../types/User";
+import * as Yup from "yup";
+import { validation } from "shared2";
+import { FlexDiv, InputLabelContainer } from "../Containers";
+import { Button, Input } from "@chakra-ui/react";
+
+const validationSchema = Yup.object().shape({
+  name: validation.nameSchema,
+  phoneNumber: validation.phoneNumberSchema,
+  email: validation.emailSchema,
+  streetAddress: validation.streetAddressSchema,
+  city: validation.citySchema,
+  state: validation.stateSchema,
+  zip: validation.zipSchema,
+});
+
+interface Props {
+  onSubmit: (values: any) => any;
+  address: Address;
+}
+
+export const EditAddressForm = ({ onSubmit, address }: Props) => {
+  const { state: bpState } = useContext(BreakpointContext);
+
+  const mobile = bpState.bp === "mobile";
+
+  return (
+    <Formik
+      initialValues={{
+        name: address.name,
+        phoneNumber: address.phoneNumber,
+        email: address.email,
+        streetAddress: address.streetAddress,
+        city: address.city,
+        state: address.state,
+        zip: address.zip,
+      }}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({
+        handleSubmit,
+        handleBlur,
+        handleChange,
+        values,
+        isSubmitting,
+        isValid,
+        errors,
+        touched,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <FlexDiv column>
+            <InputLabelContainer
+              id="name"
+              label="Name"
+              error={errors.name && touched.name ? errors.name : undefined}
+            >
+              <Input
+                autoComplete="name"
+                id="name"
+                type="name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.name}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="phoneNumber"
+              label="Phone Number"
+              error={errors.phoneNumber && touched.phoneNumber ? errors.phoneNumber : undefined}
+            >
+              <Input
+                autoComplete="tel"
+                id="phoneNumber"
+                type="tel"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.phoneNumber}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="email"
+              label="Email"
+              error={errors.email && touched.email ? errors.email : undefined}
+            >
+              <Input
+                autoComplete="email"
+                id="email"
+                type="email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="street-address"
+              label="Street Address"
+              error={
+                errors.streetAddress && touched.streetAddress ? errors.streetAddress : undefined
+              }
+            >
+              <Input
+                autoComplete="street-address"
+                id="street-address"
+                name="streetAddress"
+                value={values.streetAddress}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isRequired
+                isInvalid={!!errors.streetAddress && touched.streetAddress}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="city"
+              label="City"
+              error={errors.city && touched.city ? errors.city : undefined}
+            >
+              <Input
+                autoComplete="address-level2"
+                id="city"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isRequired
+                isInvalid={!!errors.city && touched.city}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="state"
+              label="State"
+              error={errors.state && touched.state ? errors.state : undefined}
+            >
+              <Input
+                autoComplete="address-level1"
+                id="state"
+                name="state"
+                value={values.state}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isRequired
+                isInvalid={!!errors.state && touched.state}
+              />
+            </InputLabelContainer>
+
+            <InputLabelContainer
+              id="zip"
+              label="Zip Code"
+              error={errors.zip && touched.zip ? errors.zip : undefined}
+            >
+              <Input
+                autoComplete="postal-code"
+                id="zip"
+                name="zip"
+                value={values.zip}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isRequired
+                isInvalid={!!errors.zip && touched.zip}
+              />
+            </InputLabelContainer>
+
+            <FlexDiv flexEnd>
+              <Button
+                colorScheme="blue"
+                type="submit"
+                isDisabled={!isValid || isSubmitting}
+                isLoading={isSubmitting}
+                isFullWidth={mobile}
+                loadingText="Saving"
+                mb={"0.8rem"}
+              >
+                Save
+              </Button>
+            </FlexDiv>
+          </FlexDiv>
+        </form>
+      )}
+    </Formik>
+  );
+};
