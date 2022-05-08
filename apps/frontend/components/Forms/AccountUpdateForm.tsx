0aee9d4f025e5aa4d2d66984fc9f9ee9c4df2fe0
Veeti K <veetik@protonmail.com>
2022-05-08 19:24:58 +0300

    update account updating

--- apps/frontend/components/Forms/AccountUpdateForm.tsx
+++ apps/frontend/components/Forms/AccountUpdateForm.tsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import { validation } from "shared2";
 import * as Yup from "yup";
 import { BreakpointContext } from "../../BreakpointProvider/BreakpointProvider";
+import { useBlurCounter } from "../../hooks/useBlurCounter";
 import { UserContext } from "../../UserProvider/provider";
 import { Actions } from "../../UserProvider/types";
 import { UpdateAccountRequest } from "../../utils/Requests/Account";
@@ -19,28 +20,44 @@ const validationSchema = Yup.object().shape({
 export const AccountUpdateForm = () => {
   const { dispatch, state } = useContext(UserContext);
   const { state: bpState } = useContext(BreakpointContext);
+  const { addBlurCount, blurCount } = useBlurCounter();
 
   const mobile = bpState.bp === "mobile";
 
-  const onSubmit = async (values: any) => {
-    const notifId = toast.loading("Saving your edits");
+  const onSubmit = (values: any) =>
+    toast.promise(
+      (async () => {
+        const res = await UpdateAccountRequest(values);
 
-    const res = await UpdateAccountRequest(values);
-
-    toast.dismiss(notifId);
-
-    if (res) {
-      toast.success("Saved!");
-      dispatch({ type: Actions.SetUser, payload: res.data });
-    }
-  };
+        if (res) dispatch({ type: Actions.SetUser, payload: res.data });
+      })(),
+      {
+        loading: "Saving your edits",
+        success: "Saved!",
+        error: "Error saving your edits",
+      }
+    );
 
   return (
     <Formik initialValues={state} onSubmit={onSubmit} validationSchema={validationSchema}>
-      {({ handleSubmit, values, handleChange, isSubmitting, dirty }) => (
+      {({
+        handleSubmit,
+        handleBlur,
+        values,
+        handleChange,
+        isSubmitting,
+        dirty,
+        errors,
+        isValid,
+        touched,
+      }) => (
         <form onSubmit={handleSubmit}>
           <FlexDiv column>
-            <InputLabelContainer id="name" label="Name">
+            <InputLabelContainer
+              id="name"
+              label="Name"
+              error={errors.name && touched.name ? errors.name : undefined}
+            >
               <Input
                 autoComplete="name"
                 id="name"
@@ -48,11 +65,21 @@ export const AccountUpdateForm = () => {
                 value={values.name}
                 onChange={handleChange}
                 isRequired
+                isInvalid={!!errors.name && touched.name}
+                onBlur={(e) => {
+                  addBlurCount();
+                  if (blurCount !== 1) return;
+                  handleBlur(e);
+                }}
               />
             </InputLabelContainer>
 
             <FlexDiv column={mobile}>
-              <InputLabelContainer id="email" label="Email">
+              <InputLabelContainer
+                id="email"
+                label="Email"
+                error={errors.email && touched.email ? errors.email : undefined}
+              >
                 <Input
                   autoComplete="email"
                   id="email"
@@ -60,24 +87,31 @@ export const AccountUpdateForm = () => {
                   value={values.email}
                   onChange={handleChange}
                   isRequired
+                  onBlur={handleBlur}
+                  isInvalid={!!errors.email && touched.email}
                 />
               </InputLabelContainer>
 
-              <InputLabelContainer id="phone-number" label="Phone number">
+              <InputLabelContainer
+                id="phone-number"
+                label="Phone number"
+                error={errors.phoneNumber && touched.phoneNumber ? errors.phoneNumber : undefined}
+              >
                 <Input
                   autoComplete="tel"
                   id="phone-number"
                   name="phoneNumber"
                   value={values.phoneNumber ?? ""}
                   onChange={handleChange}
-                  isRequired
+                  onBlur={handleBlur}
+                  isInvalid={!!errors.phoneNumber && touched.phoneNumber}
                 />
               </InputLabelContainer>
             </FlexDiv>
             <FlexDiv flexEnd>
               <Button
                 colorScheme="blue"
-                disabled={!dirty || isSubmitting}
+                disabled={!dirty || !isValid || isSubmitting}
                 type="submit"
                 isLoading={isSubmitting}
                 isFullWidth={mobile}
